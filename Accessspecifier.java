ed98d28d3b71ee0e9449c7242fcbe3eb28f1494c
Muhammed-Ameen <73465363+Muhammed-Ameen@users.noreply.github.com>
2021-05-11 15:10:32 +0530

    Add files via upload

---
+++ Accessspecifier.java
@@ -0,0 +1,18 @@
+//implementing access specifier
+
+class A{  
+private int data=40;  
+void msg()
+ {
+   System.out.println("Hello java");
+ }  
+}  
+  
+public class Accessspecifier{  
+ public static void main(String args[]){  
+   A obj=new A(); 
+    //System.out.println(obj.data);
+    //Compile Time Error  
+   obj.msg();
+   }  
+}  
